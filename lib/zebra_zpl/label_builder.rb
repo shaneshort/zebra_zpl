1897db32d27f4ecfd3bd8ddfe3567d56c0a555a9
BM5k <me@bm5k.com>
2012-03-16 10:16:41 -0600

    use method missing to enable basic builder functionality

--- lib/zebra_zpl/label_builder.rb
+++ lib/zebra_zpl/label_builder.rb
@@ -6,4 +6,14 @@ class ZebraZpl::LabelBuilder
     @label = ZebraZpl::Label.new
   end
 
+  def method_missing name, *args
+    return super unless label.respond_to? "#{ name }="
+
+    self.class.send :define_method, "#{ name }" do |*args|
+      label.send "#{ name }=", *args
+    end
+
+    send "#{ name }", *args
+  end
+
 end
--- spec/label_builder_spec.rb
+++ spec/label_builder_spec.rb
@@ -5,6 +5,45 @@ describe ZebraZpl::LabelBuilder do
   it 'returns a label' do
     ZebraZpl::Label.build {}.should be_a(ZebraZpl::Label)
   end
+
+  describe '#default_width' do
+
+    it 'adds an Bar Code Field Default to the label' do
+      ZebraZpl::Label.build { default_width 3 }.to_s.should =~ /\^BY3/
+    end
+
+  end
+
+  describe '#home' do
+
+    it 'adds a Label Home command to the label' do
+      ZebraZpl::Label.build { home [5, 10] }.to_s.should =~ /\^LH5,10/
+    end
+
+  end
+
+  describe '#orientation' do
+
+    it 'adds a Field Orientation command to the label' do
+      ZebraZpl::Label.build { orientation :n }.to_s.should =~ /\^FWN/
+    end
+
+  end
+
+  describe '#print_rate' do
+
+    it 'adds an Bar Code Field Default to the label' do
+      ZebraZpl::Label.build { print_rate :a }.to_s.should =~ /\^PRA/
+    end
+
+  end
+
+  describe '#quantity' do
+
+    it 'adds an Bar Code Field Default to the label' do
+      ZebraZpl::Label.build { quantity 4 }.to_s.should =~ /\^PQ4/
+    end
+
   end
 
 end
